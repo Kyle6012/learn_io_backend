71bd07aa537624da66fef3d1585491b12ad30c22
Tenkph <60466511+Tenkph@users.noreply.github.com>
2025-03-01 04:38:49 +0300

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const { notificationController } = require('../controllers/notificationController');
 const { authenticateUser, authorizeRoles } = require('../middlewares/authourizationMiddleware');
 
 /**
