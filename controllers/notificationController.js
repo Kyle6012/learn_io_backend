93d519eae8dd05be10faeeec3e0c8e89a424a5d9
Tenkph <60466511+Tenkph@users.noreply.github.com>
2025-03-01 04:32:25 +0300

    Update notificationController.js

---
+++ controllers/notificationController.js
@@ -0,0 +1,28 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com', // Change to your SMTP provider
+  port: 465, // 587 for TLS, 465 for SSL
+  secure: true, // Use TLS
+  auth: {
+    user: 'kennedygreat36@gmail.com', //
+    pass: 'dmcc mhxa oqmj yrhw', // Replace with your actual password or API key
+  },
+});
+
+const mailOptions = {
+  from: 'kennedygreat36@gmail.com', // Your email
+  to: 'douglasdollars900@gmail.com', // Replace with recipient email
+  subject: 'Hello from Node.js SMTP!',
+  text: 'Tragaph was here',
+};
+
+transporter.sendMail(mailOptions, (error, info) => {
+  if (error) {
+    console.error('❌ Error sending email:', error);
+  } else {
+    console.log('✅ Email sent:', info.response);
+  }
+});
+
+module.exports = { transporter };
