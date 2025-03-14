119124c685e06038f1e9d86a6b9c64e2f9593e5e
brandonladen <brandonladen486@gmail.com>
2025-03-15 00:03:29 +0300

    resolved conflicts

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const { sendEmail } = require('../controllers/notificationController');
-const { authenticateUser, authorizeRoles } = require('../middlewares/authourizationMiddleware');
 const { authenticateUser, authorizeRoles, validateRoles } = require('../middlewares/authourizationMiddleware');
 
 /**
